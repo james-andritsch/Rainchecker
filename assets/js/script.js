4d11b53357f25551bf755d49f73180cb03ab9d5a
James <andritjm@gmail.com>
2023-03-05 00:56:15 -0600

    tried to add 5-day forecast but not functional yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,33 @@ function mainPage() {
         });
 
         let locationID = response.data.id;
-        
+        let forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?id=" + locationID + "&appid=" + apiKey;
+        axios.get(forecastQuery)
+        .then(function(response){
+            console.log(response);
+            const fData = document.querySelectorAll(".forecast");
+            for (i=0; i<forecastData.length; i++) {
+                fData[i].innerHTML = "";
+                const fIndex = i*8 + 4;
+                const fDate = new Date(response.data.list[fIndex].dt * 1000);
+                const fDay = fDate.getDate();
+                const fMonth = fDate.getMonth() + 1;
+                const fYear = fDate.getFullYear();
+                const fDateElement = document.createElement("p");
+                fDateElement.setAttribute("class","mt-3 mb-0");
+                fDateElement.innerHTML = fMonth + "/" + fDay + "/" + fYear;
+                fData[i].append(fDateElement);
+                // 5 day forecast
+                const fWeatherElement = document.createElement("img");
+                fWeatherElement.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[fIndex].weather[0].icon + "@2x.png");
+                fWeatherElement.setAttribute("alst",response.data.list[fIndex].weather[0].description);
+                fData[i].append(fWeatherElement);
+                const fTempElement = document.createElement("p");
+                fTempElement.innerHTML = "Temp: " + k2f(response.data.list[fIndex].main.temp) + " &#176F";
+                fData[i].append(fTempElement);
+                
+            }
+        })
     }
     
 };
